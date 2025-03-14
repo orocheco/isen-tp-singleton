0cf5cef618a42bbf1df08da2e9ea1a69198c0c55
orochecouste <orochecouste@tech-and-dev.fr>
2025-03-14 10:19:14 +0100

    First release

--- main.cpp
+++ main.cpp
@@ -4,16 +4,7 @@
 #include "MonSingletonSansPointeur.h"
 
 int main() {
-    MonSingleton::getInstance()->showMessage();
-
-    auto s1 = MonSingleton::getInstance();
-    auto s2 = MonSingleton::getInstance();
-
-    std::cout << "adresse s1: " << s1 << std::endl;
-    std::cout << "adresse s2: " << s2 << std::endl;
-
-    MonSingletonSansPointeur::getInstance().showMessage();
-
+    /* à compléter ici */
 
     return 0;
 }
